a984157444625d306e7963e3dd17fdb84da57398
Kenan Kadic <kkadic@midtownhi.com>
2022-02-17 14:54:04 -0600

    title added

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxt-testing2',
-    title: 'nuxt-testing2',
+    titleTemplate: '%s - nuxt-contact-form',
+    title: 'nuxt-contact-form',
     htmlAttrs: {
       lang: 'en'
     },
